e3156abf0b236fd16c3720b9bb9be3d1841327f4
Rob Herring <robh@kernel.org>
2018-01-17 18:07:27 -0600

    Generate schema selection from compatible definition
    For most nodes we match against their compatible property. Since the
    compatible property schema already contains all the possible values, extract
    them from the schema and generate a selection schema. This avoids having
    to duplicate compatible strings.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- dt-validate.py
+++ dt-validate.py
@@ -6,6 +6,35 @@ import jsonschema
 import argparse
 import glob
 
+def item_generator(json_input, lookup_key):
+    if isinstance(json_input, dict):
+        for k, v in json_input.items():
+            if k == lookup_key:
+                yield v
+            else:
+                for child_val in item_generator(v, lookup_key):
+                    yield child_val
+    elif isinstance(json_input, list):
+        for item in json_input:
+            for item_val in item_generator(item, lookup_key):
+                yield item_val
+
+def compatible_select(schema):
+    compatible_list = [ ]
+    if not 'properties' in schema.keys():
+        return None
+
+    if 'compatible' in schema['properties'].keys():
+        for l in item_generator(schema['properties']['compatible'], 'enum'):
+            for _l in l:
+                if _l not in compatible_list:
+                    compatible_list.append(_l)
+
+        return { 'required' : ['compatible'], 'properties': {'compatible': {'contains': {'enum': compatible_list}}}}
+
+    return None
+
+
 class schema_group():
     def __init__(self):
         self.schemas = list()
@@ -38,6 +67,12 @@ class schema_group():
                 return
 
         schema["filename"] = filename
+
+        select = compatible_select(schema)
+        if select is not None:
+            print("no select")
+            schema["select"] = select
+
         self.schemas.append(schema)
         print("loaded schema", filename)
 
