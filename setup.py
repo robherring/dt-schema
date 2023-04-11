188cca9ae852edf1f21906c6a2912d52dd90144b
Rob Herring <robh@kernel.org>
2023-04-11 17:21:49 -0500

    Limit jsonschema version to less than 4.18
    The current alpha for jsonschema 4.18 breaks our custom reference
    resolution. The API is going to be deprecated as well. Limit the version
    until we know jsonschema is fixed or dtschema is updated.
    
    From now on, pin the max version because this wouldn't be the first time
    a minor version broke us. :(
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
 
     install_requires=[
         'ruamel.yaml>0.15.69',
-        'jsonschema>=4.1.2',
+        'jsonschema >=4.1.2, <4.18',
         'rfc3987',
         'pylibfdt',
     ],
